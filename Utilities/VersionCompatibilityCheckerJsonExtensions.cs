0a8e1b43c4653484a162b116cf4e3486472451ca
Vladyslav Zaiets <rutova2@gmail.com>
2023-03-17 08:56:24 +0200

    add VersionCompatibilityCheckerJson json helpers

---
+++ Utilities/VersionCompatibilityCheckerJsonExtensions.cs
@@ -0,0 +1,88 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SkiaSharpChartEngine.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="VersionCompatibilityChecker"/>.
+/// </summary>
+public static class VersionCompatibilityCheckerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="VersionCompatibilityChecker"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The version compatibility checker instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the version compatibility checker.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this VersionCompatibilityChecker value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="VersionCompatibilityChecker"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A deserialized <see cref="VersionCompatibilityChecker"/> instance, or null if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static VersionCompatibilityChecker? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<VersionCompatibilityChecker>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="VersionCompatibilityChecker"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful, otherwise null.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out VersionCompatibilityChecker? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<VersionCompatibilityChecker>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
