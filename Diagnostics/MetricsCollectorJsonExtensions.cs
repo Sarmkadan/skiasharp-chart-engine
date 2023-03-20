b4f2583babc02c45fe17635625db3520cd835ac0
Vladyslav Zaiets <rutova2@gmail.com>
2023-03-20 11:07:13 +0200

    json round-trip support for MetricsCollectorJson

---
+++ Diagnostics/MetricsCollectorJsonExtensions.cs
@@ -0,0 +1,89 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SkiaSharpChartEngine.Diagnostics;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="MetricsCollector"/>.
+/// </summary>
+public static class MetricsCollectorJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Converts the <see cref="MetricsCollector"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The metrics collector to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON representation of the metrics collector.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this MetricsCollector value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="MetricsCollector"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A deserialized <see cref="MetricsCollector"/> instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static MetricsCollector? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<MetricsCollector>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="MetricsCollector"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized <see cref="MetricsCollector"/> instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out MetricsCollector? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<MetricsCollector>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
