933b04092acf5b323f448ba6707600f89ea3b382
Vladyslav Zaiets <rutova2@gmail.com>
2023-04-25 12:04:44 +0300

    feat: add extension helpers for CreateChartRequest.

--- API/Requests/CreateChartRequestExtensions.cs
+++ API/Requests/CreateChartRequestExtensions.cs
@@ -1,22 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SkiasharpChartEngine.API.Requests
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="CreateChartRequest"/> to simplify common operations.
+    /// </summary>
     public static class CreateChartRequestExtensions
     {
         /// <summary>
         /// Validates the chart request and throws an exception if it's invalid.
         /// </summary>
         /// <param name="request">The chart request to validate.</param>
-        /// <exception cref="ArgumentException">Thrown if the request is invalid.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">The request is invalid.</exception>
         public static void Validate(this CreateChartRequest request)
         {
-            if (request == null)
-            {
-                throw new ArgumentNullException(nameof(request));
-            }
+            ArgumentNullException.ThrowIfNull(request);
 
             if (!request.IsValid)
             {
@@ -25,22 +25,26 @@ namespace SkiasharpChartEngine.API.Requests
         }
 
         /// <summary>
-        /// Returns a list of chart series with default values if the series are null.
+        /// Returns the series collection, ensuring it's never null.
         /// </summary>
         /// <param name="request">The chart request.</param>
-        /// <returns>A list of chart series.</returns>
-        public static List<ChartSeries> GetSeriesOrDefault(this CreateChartRequest request)
+        /// <returns>The series collection, or an empty list if null.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+        public static IReadOnlyList<ChartSeries> GetSeriesOrDefault(this CreateChartRequest request)
         {
-            return request.Series ?? new List<ChartSeries>();
+            ArgumentNullException.ThrowIfNull(request);
+            return request.Series ?? [];
         }
 
         /// <summary>
-        /// Returns the chart configuration with default values if the configuration is null.
+        /// Returns the chart configuration, ensuring it's never null.
         /// </summary>
         /// <param name="request">The chart request.</param>
-        /// <returns>The chart configuration.</returns>
+        /// <returns>The chart configuration, or a new instance if null.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
         public static ChartConfiguration GetConfigurationOrDefault(this CreateChartRequest request)
         {
+            ArgumentNullException.ThrowIfNull(request);
             return request.Configuration ?? new ChartConfiguration();
         }
     }
