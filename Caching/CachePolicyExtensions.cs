518a5d666f1c3a5a95d74c324ebdd8058982d609
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-04 15:28:59 +0300

    cleanup

--- Caching/CachePolicyExtensions.cs
+++ Caching/CachePolicyExtensions.cs
@@ -8,51 +8,52 @@ using System;
 namespace SkiaSharpChartEngine.Caching;
 
 /// <summary>
-/// Extension methods for CachePolicy providing convenient helper methods
+/// Extension methods for <see cref="CachePolicy"/> providing convenient helper methods
+/// for configuring cache expiration and behavior.
 /// </summary>
 public static class CachePolicyExtensions
 {
     /// <summary>
-    /// Sets the cache priority for this policy
+    /// Sets the cache priority for this policy.
     /// </summary>
-    /// <param name="policy">The cache policy to configure</param>
-    /// <param name="priority">The priority level</param>
-    /// <returns>The configured cache policy for method chaining</returns>
+    /// <param name="policy">The cache policy to configure. Cannot be <see langword="null"/>.</param>
+    /// <param name="priority">The priority level to set.</param>
+    /// <returns>The configured cache policy for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="policy"/> is <see langword="null"/>.</exception>
     public static CachePolicy WithPriority(this CachePolicy policy, CachePriority priority)
     {
-        if (policy == null)
-            throw new ArgumentNullException(nameof(policy));
+        ArgumentNullException.ThrowIfNull(policy);
 
         policy.Priority = priority;
         return policy;
     }
 
     /// <summary>
-    /// Sets a post-eviction callback for this policy
+    /// Sets a post-eviction callback for this policy.
     /// </summary>
-    /// <param name="policy">The cache policy to configure</param>
-    /// <param name="callback">The callback to invoke when cache item is evicted</param>
-    /// <returns>The configured cache policy for method chaining</returns>
-    public static CachePolicy WithPostEvictionCallback(this CachePolicy policy, Action<string> callback)
+    /// <param name="policy">The cache policy to configure. Cannot be <see langword="null"/>.</param>
+    /// <param name="callback">The callback to invoke when a cache item is evicted. Can be <see langword="null"/> to clear any existing callback.</param>
+    /// <returns>The configured cache policy for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="policy"/> is <see langword="null"/>.</exception>
+    public static CachePolicy WithPostEvictionCallback(this CachePolicy policy, Action<string>? callback)
     {
-        if (policy == null)
-            throw new ArgumentNullException(nameof(policy));
+        ArgumentNullException.ThrowIfNull(policy);
 
         policy.PostEvictionCallbacks = callback;
         return policy;
     }
 
     /// <summary>
-    /// Sets both absolute and sliding expiration times
+    /// Sets both absolute and sliding expiration times.
     /// </summary>
-    /// <param name="policy">The cache policy to configure</param>
-    /// <param name="absoluteExpiration">Absolute expiration time span</param>
-    /// <param name="slidingExpiration">Sliding expiration time span</param>
-    /// <returns>The configured cache policy for method chaining</returns>
+    /// <param name="policy">The cache policy to configure. Cannot be <see langword="null"/>.</param>
+    /// <param name="absoluteExpiration">Absolute expiration time span. Can be <see langword="null"/> for no absolute expiration.</param>
+    /// <param name="slidingExpiration">Sliding expiration time span. Can be <see langword="null"/> for no sliding expiration.</param>
+    /// <returns>The configured cache policy for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="policy"/> is <see langword="null"/>.</exception>
     public static CachePolicy WithExpiration(this CachePolicy policy, TimeSpan? absoluteExpiration, TimeSpan? slidingExpiration)
     {
-        if (policy == null)
-            throw new ArgumentNullException(nameof(policy));
+        ArgumentNullException.ThrowIfNull(policy);
 
         policy.AbsoluteExpirationRelativeToNow = absoluteExpiration;
         policy.SlidingExpiration = slidingExpiration;
@@ -60,15 +61,16 @@ public static class CachePolicyExtensions
     }
 
     /// <summary>
-    /// Clamps the sliding expiration to be no more than 80% of absolute expiration
-    /// to prevent premature eviction
+    /// Ensures the sliding expiration is valid relative to absolute expiration.
+    /// If sliding expiration exceeds absolute expiration, it is clamped to 80% of absolute expiration
+    /// to prevent premature eviction and ensure cache entries remain valid for their intended duration.
     /// </summary>
-    /// <param name="policy">The cache policy to validate and adjust</param>
-    /// <returns>The policy with clamped sliding expiration</returns>
+    /// <param name="policy">The cache policy to validate and adjust. Cannot be <see langword="null"/>.</param>
+    /// <returns>The policy with clamped sliding expiration.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="policy"/> is <see langword="null"/>.</exception>
     public static CachePolicy EnsureValidSlidingExpiration(this CachePolicy policy)
     {
-        if (policy == null)
-            throw new ArgumentNullException(nameof(policy));
+        ArgumentNullException.ThrowIfNull(policy);
 
         if (policy.AbsoluteExpirationRelativeToNow.HasValue && policy.SlidingExpiration.HasValue)
         {
@@ -76,6 +78,7 @@ public static class CachePolicyExtensions
             var sliding = policy.SlidingExpiration.Value;
 
             // Ensure sliding expiration is not greater than absolute expiration
+            // Clamp to 80% of absolute expiration to maintain reasonable cache retention
             if (sliding > absolute)
             {
                 policy.SlidingExpiration = TimeSpan.FromTicks((long)(absolute.Ticks * 0.8));
