c63b469d5612de881ff0129a9832566b440f5667
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-04 12:24:45 +0300

    feat(streaming): add extension helpers for Streaming

--- Streaming/StreamingServiceExtensions.cs
+++ Streaming/StreamingServiceExtensions.cs
@@ -16,12 +16,21 @@ public static class StreamingServiceExtensions
     /// Registers <see cref="IChartStreamingService"/> (and its default implementation
     /// <see cref="ChartStreamingService"/>) as a singleton in the service collection.
     /// </summary>
+    /// <remarks>
+    /// The registered service requires the following dependencies to be available in the container:
+    /// <list type="bullet">
+    ///   <item><description><see cref="Microsoft.Extensions.Logging.ILogger{ChartStreamingService}"/></description></item>
+    ///   <item><description><see cref="SkiaSharpChartEngine.Services.IChartRenderingService"/></description></item>
+    /// </list>
+    /// </remarks>
     /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
     /// <returns>The same <see cref="IServiceCollection"/> for fluent chaining.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="services"/> is <c>null</c>.
+    /// </exception>
     public static IServiceCollection AddChartStreaming(this IServiceCollection services)
     {
-        if (services == null) throw new ArgumentNullException(nameof(services));
+        ArgumentNullException.ThrowIfNull(services);
 
         services.AddSingleton<IChartStreamingService, ChartStreamingService>();
         return services;
