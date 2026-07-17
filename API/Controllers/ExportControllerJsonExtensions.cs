20e34782aba056485404c8811ccfc6f2c3d1207f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:36:47 +0300

    Dedupe some logic in ExportControllerJsonExtensions
    found this while looking at something else.

--- API/Controllers/ExportControllerJsonExtensions.cs
+++ API/Controllers/ExportControllerJsonExtensions.cs
@@ -29,7 +29,8 @@ public static class ExportControllerJsonExtensions
     /// <param name="value">The ExportController instance to serialize</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability</param>
     /// <returns>A JSON string representation of the ExportController</returns>
-    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+    /// <exception cref="JsonException">Thrown when serialization fails</exception>
     public static string ToJson(this ExportController value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -46,9 +47,12 @@ public static class ExportControllerJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize</param>
     /// <returns>The deserialized ExportController instance, or null if JSON is null or empty</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null</exception>
     /// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized</exception>
     public static ExportController? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrEmpty(json))
         {
             return null;
@@ -63,12 +67,26 @@ public static class ExportControllerJsonExtensions
     /// <param name="json">The JSON string to deserialize</param>
     /// <param name="value">Receives the deserialized ExportController instance if successful</param>
     /// <returns>True if deserialization succeeded; otherwise, false</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null</exception>
     public static bool TryFromJson(string json, out ExportController? value)
+        => TryFromJson(json, out value, out _);
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an ExportController instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized ExportController instance if successful</param>
+    /// <param name="errorMessage">Receives the error message if deserialization fails</param>
+    /// <returns>True if deserialization succeeded; otherwise, false</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null</exception>
+    public static bool TryFromJson(string json, out ExportController? value, out string? errorMessage)
     {
         value = null;
+        errorMessage = null;
 
         if (string.IsNullOrEmpty(json))
         {
+            errorMessage = "JSON string is null or empty";
             return false;
         }
 
@@ -77,8 +95,9 @@ public static class ExportControllerJsonExtensions
             value = JsonSerializer.Deserialize<ExportController>(json, _jsonOptions);
             return true;
         }
-        catch (JsonException)
+        catch (JsonException ex)
         {
+            errorMessage = ex.Message;
             return false;
         }
     }
