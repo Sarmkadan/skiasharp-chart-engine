1b0de2aedd309cfe6761d1dd0653f9d8cf8ca454
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 09:03:26 +0300

    polish TemplateControllerExtensions

--- API/Controllers/TemplateControllerExtensions.cs
+++ API/Controllers/TemplateControllerExtensions.cs
@@ -11,10 +11,12 @@ public static class TemplateControllerExtensions
     /// <param name="controller">The <see cref="TemplateController"/> instance.</param>
     /// <param name="templateId">The ID of the template to check.</param>
     /// <returns>True if the template exists, false otherwise.</returns>
-    /// <exception cref="ArgumentException">Thrown if <paramref name="templateId"/> is invalid.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="controller"/> is null.</exception>
+/// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="templateId"/> is less than or equal to 0.</exception>
     public static async Task<bool> TemplateExistsAsync(this TemplateController controller, int templateId)
     {
-        ArgumentException.ThrowIfNullOrEmpty(templateId.ToString(CultureInfo.InvariantCulture));
+        ArgumentNullException.ThrowIfNull(controller);
+	ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(templateId, 0);
 
         var response = await controller.GetTemplateById(templateId);
         return response.IsSuccess && response.Value is not null;
@@ -26,10 +28,12 @@ public static class TemplateControllerExtensions
     /// <param name="controller">The <see cref="TemplateController"/> instance.</param>
     /// <param name="templateType">The type of templates to delete.</param>
     /// <returns>A task that completes when all templates have been deleted.</returns>
-    /// <exception cref="ArgumentException">Thrown if <paramref name="templateType"/> is invalid.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="controller"/> is null.</exception>
+/// <exception cref="ArgumentException">Thrown if <paramref name="templateType"/> is null or empty.</exception>
     public static async Task DeleteAllTemplatesOfTypeAsync(this TemplateController controller, string templateType)
     {
-        ArgumentException.ThrowIfNullOrEmpty(templateType);
+        ArgumentNullException.ThrowIfNull(controller);
+	ArgumentException.ThrowIfNullOrEmpty(templateType);
 
         var templates = await controller.GetTemplatesByType(templateType);
         if (templates.IsSuccess && templates.Value is not null)
