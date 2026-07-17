6086e90acbceb599190ee7168b08133d55859d79
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 08:36:04 +0300

    refactor: ploish ChartEngineJsonExtensions

--- ChartEngineJsonExtensions.cs
+++ ChartEngineJsonExtensions.cs
@@ -16,79 +16,83 @@ namespace SkiaSharpChartEngine;
 /// </summary>
 public static class ChartEngineJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
-            {
-                Modifiers = {
-                    static typeInfo => {
-                        if (typeInfo.Type == typeof(ChartEngine))
-                        {
-                            typeInfo.PolymorphismOptions = new JsonPolymorphismOptions
-                            {
-                                TypeDiscriminatorPropertyName = "$type"
-                            };
-                        }
-                    }
-                }
-            }
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+		{
+			Modifiers = {
+				static typeInfo => {
+					if (typeInfo.Type == typeof(ChartEngine))
+					{
+						typeInfo.PolymorphismOptions = new JsonPolymorphismOptions
+						{
+							TypeDiscriminatorPropertyName = "$type"
+						};
+					}
+				}
+			}
+		}
+	};
 
-    /// <summary>
-    /// Serializes the <see cref="ChartEngine"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The ChartEngine instance to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the ChartEngine.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this ChartEngine value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the <see cref="ChartEngine"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The ChartEngine instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the ChartEngine.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this ChartEngine value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions)
+			{
+				WriteIndented = true
+			}
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="ChartEngine"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A ChartEngine instance, or null if the JSON represents a null value.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static ChartEngine? FromJson(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="ChartEngine"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>A ChartEngine instance, or null if the JSON represents a null value.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static ChartEngine? FromJson(string json)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
-        return JsonSerializer.Deserialize<ChartEngine>(json, _jsonOptions);
-    }
+		return JsonSerializer.Deserialize<ChartEngine>(json, _jsonOptions);
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="ChartEngine"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized ChartEngine instance, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out ChartEngine? value)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="ChartEngine"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized ChartEngine instance, or null if deserialization fails.</param>
+	/// <returns>True if deserialization succeeds; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+	public static bool TryFromJson(string json, out ChartEngine? value)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
-        try
-        {
-            value = JsonSerializer.Deserialize<ChartEngine>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+		try
+		{
+			value = JsonSerializer.Deserialize<ChartEngine>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
